5d4a3a9875a126bfd74b52fb474d9f108b6c555a
Padmalaya Singhai <61295498+paddySinghai@users.noreply.github.com>
2024-05-13 15:42:28 +0200

    initial commit

---
+++ src/test/java/com/nl/retention/RetentionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nl.retention;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetentionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
